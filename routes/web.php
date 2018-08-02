609df0b2e3cdaf4ab3ccbadd49e7a031de8be8e6
aFtBfF <1721960143@qq.com>
2018-08-02 06:17:33 +0000

    Finish user signup

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 //注册路由时，URI signup 和 /signup 从使用上来看，并无区别，Laravel 框架兼容这两种写法。
+
+Route::resource('users', 'UsersController');
